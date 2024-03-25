c356a8d058345fdf69c2b27a839b71583ededecd
hildelio <hildelio@gmail.com>
2024-03-25 20:05:09 -0300

    Criando componentes e estilizando

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,16 @@ function Header() {
   const firstName = 'Zé';
   const lastName = 'Pequeno';
   return (
-    <div className="header-content">
-      <div className="avatar-content">
+    <header className="header-content">
+      <section className="avatar-content">
         <img className="avatar" src="https://img.freepik.com/psd-gratuitas/ilustracao-3d-de-avatar-ou-perfil-humano_23-2150671116.jpg?size=626&ext=jpg&ga=GA1.1.1741435561.1709756962&semt=sph" alt="avatar"/>
-        <h1 className="greeting">
+        <p className="greeting">
           Olá, {`${firstName} ${lastName}`}
-        </h1>
-      </div>
+        </p>
+      </section>
       <h1>Revisando React</h1>
       <img className="logo" src="https://i.imgur.com/rqvLd3q.png" alt="logo"/>      
-    </div>
+    </header>
   );
 }
 
