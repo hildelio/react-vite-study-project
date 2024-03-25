67f17506ef9c7cef290230a75bb5a747eef34604
hildelio <hildelio@gmail.com>
2024-03-25 15:55:31 -0300

    subindo projeto no repositorio

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h3>Hello World</h3>
+      <h3>Hello World! </h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
